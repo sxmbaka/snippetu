3a691c81cd44b3b4dc2d513277306e8297477476
Yatharth Singh <yatharth.singhhh@gmail.com>
2024-03-18 17:45:22 +0530

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module sxmbaka.snippetu
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		log.Println("404 Not Found at", r.RequestURI)
+		return
+	}
+
+	w.Write([]byte("Hello From SNIPPETU"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create new snippet"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	log.Print("Server starting at 4000")
+	err := http.ListenAndServe("localhost:4000", mux)
+	log.Fatal(err)
+}
