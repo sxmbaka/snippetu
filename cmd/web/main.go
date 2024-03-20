69d1e4e73bc2e597310fb56acfb898ca34900e26
Yatharth Singh <yatharth.singhhh@gmail.com>
2024-03-20 16:40:50 +0530

    Added neuter function as middleware to supress directory listing from the file server.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,9 +7,13 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.Handle("/static/", http.StripPrefix("/static", neuter(fileServer)))
 
 	log.Println("Staring server at http://localhost:4000")
 	err := http.ListenAndServe(":4000", mux)
---
+++ cmd/web/neuter.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+)
+
+func neuter(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasSuffix(r.URL.Path, "/") {
+			http.NotFound(w, r)
+
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
