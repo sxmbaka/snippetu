7f2d268507726b8c9138eea078ec59e8f29c27ba
Yatharth Singh <yatharth.singhhh@gmail.com>
2024-04-01 13:57:56 +0530

    Added log levels

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,11 +4,24 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP Network Address")
 	flag.Parse()
+
+	// Use log.New() to create a logger for writing information messages. This takes
+	// three parameters: the destination to write the logs to (os.Stdout), a string
+	// prefix for message (INFO followed by a tab), and flags to indicate what
+	// additional information to include (local date and time). Note that the flags
+	// are joined using the bitwise OR operator |.
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	
+	// Create a logger for writing error messages in the same way, but use stderr as
+	// the destination and use the log.Lshortfile flag to include the relevant
+	// file name and line number.
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	mux := http.NewServeMux()
 
@@ -19,7 +32,7 @@ func main() {
 	mux.HandleFunc("/snippet/create", snippetCreate)
 	mux.Handle("/static/", http.StripPrefix("/static", neuter(fileServer)))
 
-	log.Println("Staring server at http://localhost" + *addr)
+	infoLog.Println("Staring server at http://localhost" + *addr)
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	errorLog.Fatal(err)
 }
